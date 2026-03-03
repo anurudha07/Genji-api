b0cf11b2a2c97948e5fbdc0e2de3e6d10d32000c
Anurudha Sarkar <anurudhs567@gmail.com>
2026-03-03 22:58:41 +0530

    fix(db): use env config for MONGO_URI

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import env from "./env";
 
 dotenv.config();
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI!);
+    const conn = await mongoose.connect(env.MONGO_URI!);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
         const errorMessage = err instanceof Error 
